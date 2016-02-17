b9cce60db6bbe6f67c4f07a5e652714e6b07b39d
Jenkins <jenkins@jenkins-us-west-1.int.auth0.com>
2016-02-17 15:01:14 +0000

    3.1.4

---
+++ build/landing/js/index.js
@@ -0,0 +1,184 @@
+
+
+
+function playground() {
+  var $elems = $('.js-playground');
+  var count = $elems.length;
+
+  $elems.find('[data-toggle="tab"]').on('shown.bs.tab', refreshCodeMirrorInTab);
+
+  function setMode(e) {
+    e.preventDefault();
+
+    var $pill = $(this);
+    var isDark = ($pill.attr('data-set-mode') === 'dark');
+
+    $(this).closest('ul').find('li').removeClass('active');
+    $(this).closest('li').addClass('active');
+    $(this).closest('.js-playground').toggleClass('theme-dark', isDark);
+  }
+
+  function refreshCodeMirrorInTab(e) {
+    var $elem = $($(this).attr('href')).find('.CodeMirror');
+
+    if(!$elem.length) {
+      return;
+    };
+
+    $elem.get(0).CodeMirror.refresh();
+  }
+
+  function stripIndent(str) {
+  	var match = str.match(/^[ \t]*(?=\S)/gm);
+
+  	if (!match) {
+  		return str;
+  	}
+
+  	var indent = Math.min.apply(Math, match.map(function (el) {
+  		return el.length;
+  	}));
+
+  	var re = new RegExp('^[ \\t]{' + indent + '}', 'gm');
+
+  	return indent > 0 ? str.replace(re, '') : str;
+  }
+
+  $elems.each(function(i) {
+    var $component = $(this);
+
+    if(!$component.attr('data-src')) {
+      return;
+    }
+
+    var path = $component.attr('data-src');
+
+    var $canvas = $component.find('.playground-canvas');
+    var $html = $component.find('[data-lang="html"] textarea');
+
+    $html.val(stripIndent($canvas.html()));
+
+
+    $component.on('click', '.nav-pills a', setMode);
+
+    $.get(path + '.jade', function(contents) {
+      var $jade = $component.find('[data-lang="jade"] textarea');
+
+      $jade.val(contents);
+
+      CodeMirror.fromTextArea($jade.get(0), {
+        lineNumbers: true,
+        readOnly: true,
+        theme: 'auth0',
+        mode: 'jade'
+      });
+
+      CodeMirror.fromTextArea($html.get(0), {
+        lineNumbers: true,
+        readOnly: true,
+        theme: 'auth0',
+        mode: 'text/html'
+      });
+    });
+  });
+}
+
+playground();
+
+
+function navigation() {
+  $(window).on('hashchange', setSelected);
+
+  function build() {
+    var $nav = $('.nav-styleguide ul');
+
+    $nav.html('');
+
+    $('[data-group]').each(function(i) {
+      var $group = $(this);
+      var name = $group.attr('data-group');
+      var id = $group.attr('id');
+      var tpl = [
+        '<li data-accordion class="'+ ((i === 0) ? 'open' : '') +'"><a class="'+ ((i === 0) ? 'is-current' : '') +'" href="#'+ id +'">',
+        name,
+        '</a></li>'
+      ].join('');
+
+      var $item = $(tpl);
+
+      if($group.find('h2[id]').length) {
+        var $list = $('<ul></ul>');
+
+        $group.find('h2[id]').each(function(i) {
+          var name = $(this).text();
+          var id = $(this).attr('id') || 'foo';
+
+          $list.append('<li><a href="#'+ id + '">' + name + '</li>');
+
+          $item.append($list);
+        });
+      }
+
+      return $nav.append($item);
+    });
+  }
+
+  function setSelected(hash) {
+    var hash = location.hash || hash;
+    var activeClass = 'is-current';
+    var $section = $(hash + '[data-group]');
+    var $subSection = $(hash).closest('[data-group]');
+    var $navItem = $('.nav-styleguide a[href="' + hash + '"]');
+
+    if($('[data-group], .nav-styleguide a').length) {
+      $('[data-group], .nav-styleguide a').removeClass(activeClass);
+    }
+
+    $('#menu').collapse('hide');
+
+    if(hash.length) {
+      $('[data-accordion]').removeClass('open');
+    }
+
+    if($section.length) {
+      $section.addClass(activeClass);
+
+      $(window).scrollTop(0);
+    } else if ($subSection.length) {
+      $subSection.addClass(activeClass);
+    }
+
+    $navItem.addClass(activeClass);
+    $navItem.closest('[data-accordion]').addClass('open');
+  }
+
+  build();
+
+  setSelected(location.hash);
+}
+
+navigation();
+
+function accordions() {
+  $('.nav-styleguide [data-accordion]').accordion({
+    controlElement: 'a',
+    contentElement: 'ul'
+  });
+}
+
+accordions();
+
+function snippets() {
+  hljs.configure({
+    tabReplace: '  ', // 4 spaces
+    classPrefix: ''     // don't append class prefix
+                        // … other options aren't changed
+  });
+  hljs.initHighlighting();
+
+  $('pre.hl code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+}
+
+snippets();

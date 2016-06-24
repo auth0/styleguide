621aa67e4f720cad33f3f6973f610237580a45d8
Victor Fernández <victorhf@gmail.com>
2016-06-24 12:29:58 -0300

    Add support for templating via EJS in emails

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
+const ejs = require('gulp-ejs');
 const mjmlCompile = require('gulp-mjml');
 const mjml = require('mjml');
 const $ = require('gulp-load-plugins')();
@@ -84,7 +85,7 @@ gulp.task('jade-lib', function() {
     .pipe(gulp.dest('./build/landing/html/'))
 });
 
-gulp.task('jade-landing', ['mjml'], function() {
+gulp.task('jade-landing', ['emails'], function() {
   var styleguideVersion = require('./package.json').version;
 
   gulp.src('./landing/index.jade')
@@ -107,21 +108,30 @@ gulp.task('cssmin', ['stylus-lib', 'stylus-landing'], function() {
     .pipe(gulp.dest('build'))
 });
 
-gulp.task('mjml', function () {
-  return gulp.src('lib/emails/**/*.mjml')
+gulp.task('ejs', function () {
+  return gulp.src(['lib/emails/**/*.ejs'])
+    .pipe(ejs({
+      msg: "ejs + mjml rocks"
+    }))
+    .pipe(gulp.dest('build/lib/emails/'))
+});
+
+gulp.task('mjml', ['ejs'], function () {
+  return gulp.src(['build/lib/emails/**/*.ejs', '!build/lib/emails/_modules/*.ejs'])
     .pipe(mjmlCompile(mjml))
     .pipe(gulp.dest('build/lib/emails/'))
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['./lib/**/*.mjml'], ['mjml', 'jade-landing']);
+  gulp.watch(['./lib/**/*.ejs'], ['emails', 'jade-landing']);
   gulp.watch(['./lib/**/*.jade'], ['templates', 'copy']);
   gulp.watch(['./landing/**/*.jade', './landing/**/*.js'], ['jade-landing', 'copy']);
   gulp.watch(['./lib/**/*.styl'], ['css']);
   gulp.watch(['./landing/**/*.styl'], ['css']);
 });
 
-gulp.task('templates', ['jade-landing', 'jade-lib', 'mjml']);
+gulp.task('emails', ['ejs', 'mjml']);
+gulp.task('templates', ['jade-landing', 'jade-lib', 'emails']);
 gulp.task('stylus', ['stylus-landing', 'stylus-lib']);
 gulp.task('css', ['stylus', 'cssmin']);
 

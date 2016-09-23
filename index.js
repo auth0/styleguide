8d2675a31968da87e0fb5c34436e32395f7deeaf
Ariel Gerstein <ariel.ger@hotmail.com>
2016-09-23 12:38:34 -0300

    Add playground react component and react components page with react.

--- index.js
+++ index.js
@@ -1,80 +1,118 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import select from './lib/react/select/index';
-import emptyState from './lib/react/empty-state/index';
-import extensionHeader from './lib/react/extension-header/index';
+import Playground from './lib/react/playground/index';
 
-window.React = React;
+import Select from './lib/react/select/index';
+import EmptyState from './lib/react/empty-state/index';
 
-window.Styleguide = {
-  "select": select,
-  "emptyState": emptyState,
-  "extensionHeader": extensionHeader
-}
 
-window.Styleguide.create = (opts, elem) => {
-  if(!window.Styleguide[opts.component]) {
-    return;
+// window.React = React;
+
+// Init react components section as react component
+
+class ReactComponents extends React.Component {
+  render() {
+    return (
+      <section className="section content-styleguide">
+        <Playground
+          title="Select"
+          description="Select component description text."
+        >
+          <Select
+            options={[ 'Location', 'Position', 'Name' ]}
+          />
+        </Playground>
+        <Playground
+          title="Empty State"
+          description="Empty state component description text."
+        >
+          <EmptyState
+            title="Users"
+            description="Empty state example text."
+          >
+          </EmptyState>
+        </Playground>
+      </section>
+    );
   }
-
-  opts.originElementContent = [].slice.call(elem.children);
-
-  render(window.React.createElement(Styleguide[opts.component], opts), elem);
-
-  return elem.parentNode.replaceChild(elem.firstChild, elem);
 }
 
-const domIdentifier = 'styleguide';
+render(
+  <ReactComponents />,
+  document.getElementById('react-components-section')
+);
 
-const domElements = () => {
-  return document.querySelectorAll('['+ domIdentifier +']')
-};
+//
 
-const getElementOptions = (elem) => {
-  var attrs = {};
-
-  Array.prototype.slice.call(elem.attributes).forEach(function(item) {
-    var name = item.name.replace('data-', '');
-    var value = item.value;
-
-    if(item.name === domIdentifier) {
-      name = "component";
-    }
-
-    if(value.charAt(0) === '[' && value.substr(value.length - 1) === ']') {
-      value = item.value
-        .replace(/[\[\]']/g, '')
-        .replace("'", '')
-        .split(',')
-    }
-
-    if(!isNaN(value)) {
-      value = parseInt(value);
-    }
-
-    if(value === "true" || value === "false") {
-      value = (value === "true");
-    }
-
-    return attrs[name] = value;
-  });
-
-  return attrs;
-};
-
-window.Styleguide.init = () => {
-  return domElements().forEach((elem) => {
-    var opts = getElementOptions(elem);
-
-    return Styleguide.create(opts, elem);
-  });
-}
-
-window.Styleguide.initElement = (elem) => {
-  var opts = getElementOptions(elem);
-
-  return window.Styleguide.create(opts, elem);
-};
-
-window.Styleguide.init();
+// window.Styleguide = {
+//   "select": select,
+//   "emptyState": emptyState,
+//   "extensionHeader": extensionHeader
+// }
+//
+// window.Styleguide.create = (opts, elem) => {
+//   if(!window.Styleguide[opts.component]) {
+//     return;
+//   }
+//
+//   opts.originElementContent = [].slice.call(elem.children);
+//
+//   render(window.React.createElement(Styleguide[opts.component], opts), elem);
+//
+//   return elem.parentNode.replaceChild(elem.firstChild, elem);
+// }
+//
+// const domIdentifier = 'styleguide';
+//
+// const domElements = () => {
+//   return document.querySelectorAll('['+ domIdentifier +']')
+// };
+//
+// const getElementOptions = (elem) => {
+//   var attrs = {};
+//
+//   Array.prototype.slice.call(elem.attributes).forEach(function(item) {
+//     var name = item.name.replace('data-', '');
+//     var value = item.value;
+//
+//     if(item.name === domIdentifier) {
+//       name = "component";
+//     }
+//
+//     if(value.charAt(0) === '[' && value.substr(value.length - 1) === ']') {
+//       value = item.value
+//         .replace(/[\[\]']/g, '')
+//         .replace("'", '')
+//         .split(',')
+//     }
+//
+//     if(!isNaN(value)) {
+//       value = parseInt(value);
+//     }
+//
+//     if(value === "true" || value === "false") {
+//       value = (value === "true");
+//     }
+//
+//     return attrs[name] = value;
+//   });
+//
+//   return attrs;
+// };
+//
+// window.Styleguide.init = () => {
+//   return domElements().forEach((elem) => {
+//     var opts = getElementOptions(elem);
+//
+//     return Styleguide.create(opts, elem);
+//   });
+// }
+//
+// window.Styleguide.initElement = (elem) => {
+//   var opts = getElementOptions(elem);
+//
+//   return window.Styleguide.create(opts, elem);
+// };
+//
+// window.Styleguide.init();

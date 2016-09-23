206b6359590920380d9da503f0278f4c0ec9ef59
Ariel Gerstein <ariel.ger@hotmail.com>
2016-09-23 16:27:58 -0300

    Component playground for react components (live reload for testing).

--- index.js
+++ index.js
@@ -8,41 +8,53 @@ import Select from './lib/react/select/index';
 import EmptyState from './lib/react/empty-state/index';
 import LoadingPanel from './lib/react/loading-panel/index';
 
-const componentExample = require("raw!./lib/react/select/example");
-
 class ReactComponents extends React.Component {
   render() {
     return (
       <section className="section content-styleguide">
+
         <Playground
           title="Select"
           description="Select component description text."
         >
-          <ComponentPlayground codeText={componentExample} scope={{ React }} />
+          <ComponentPlayground
+            codeText="<Select options={[ 'Location', 'Position', 'Name' ]} />"
+            scope={{ React, Select }}
+          />
         </Playground>
+
         <Playground
           title="Loading panel"
           description="Loading panel component description text."
         >
-          <LoadingPanel show>
-            <div
-              style={{
-                width: '100%',
-                height: '400px',
-                backgroundColor: 'rgba(0, 0, 0, 0.2)'
-              }}
-            />
-          </LoadingPanel>
+          <ComponentPlayground
+            codeText="
+              <LoadingPanel show>
+                <div
+                  style={{
+                    width: '100%',
+                    height: '400px',
+                    backgroundColor: 'rgba(0, 0, 0, 0.2)'
+                  }}
+                />
+              </LoadingPanel>"
+            scope={{ React, LoadingPanel }}
+          />
         </Playground>
+
         <Playground
           title="Empty State"
           description="Empty state component description text."
         >
-          <EmptyState
-            title="Users"
-            description="Empty state example text."
-          >
-          </EmptyState>
+          <ComponentPlayground
+            codeText="
+            <EmptyState
+              title='Users'
+              description='Empty state example text.'
+            >
+            </EmptyState>"
+            scope={{ React, EmptyState }}
+          />
         </Playground>
       </section>
     );

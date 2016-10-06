2ea8b381844cb4314c138265ed215ded1fb31624
Ariel Gerstein <ariel.ger@hotmail.com>
2016-10-06 15:49:30 -0300

    Add select component.

--- packages/react-components/src/components/Select/Select.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Select.styl';
-
-const Select = () => <div>Select</div>;
-
-export default Select;
--- packages/react-components/src/components/Select/index.js
+++ packages/react-components/src/components/Select/index.js
@@ -1,1 +1,36 @@
-export default from './Select';
+import React, { Component, PropTypes } from 'react';
+import styles from './index.styl';
+
+const Select = (props) => {
+  const { options, selected, label, color, handleChange } = props;
+
+  return (
+    <div className={styles.select}>
+      { label && <span>{label}</span> }
+      <span className={`${styles.value} text-truncate`} style={{ color }}>{options[selected]}</span>
+      <i className="icon-budicon-460" />
+      <select onChange={handleChange} {...props}>
+        {
+          options.map((name, index) => <option key={index} value={index} selected={index === selected}>{name}</option>)
+        }
+      </select>
+    </div>
+  );
+};
+
+Select.defaultProps = {
+  options: [ 'Default' ],
+  selected: 0,
+  label: '',
+  handleChange: () => {}
+};
+
+Select.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selected: PropTypes.number.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  label: PropTypes.string,
+  color: PropTypes.string
+};
+
+export default Select;

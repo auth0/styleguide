9ab83878149e9995b57954dc75c449d7dfb0b03a
Ariel Gerstein <ariel.ger@hotmail.com>
2016-10-06 15:49:56 -0300

    Basic website structure (sidebar and content).

--- packages/react-components/tools/playground/index.js
+++ packages/react-components/tools/playground/index.js
@@ -3,11 +3,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Select } from '../../src';
+import Sidebar from './sidebar/';
 
 const Playground = () => {
   return (
-    <div>
-      <Select />
+    <div style={{ position: 'relative', height: '100%' }}>
+      <Sidebar />
+      <div style={{
+          marginLeft: '300px',
+          padding: '20px'
+        }}>
+        <Select
+          options={['Ramiro', 'Ariel', 'Victor']}
+        />
+      </div>
     </div>
   );
 };

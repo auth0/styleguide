a68bca08258c5ae86d2a411b7c62984bcade0b16
Ariel Gerstein <ariel.ger@hotmail.com>
2016-10-07 18:18:34 -0300

    Add highlight.js library

--- packages/website/src/components/Playground/index.js
+++ packages/website/src/components/Playground/index.js
@@ -1,27 +1,37 @@
 import React, { PropTypes } from 'react';
-import Highlight from 'react-highlight';
 import styles from './index.styl';
 
-const Playground = ({ children }) =>
-  <div className="react-component-playground">
-    { children }
-    <div className="react-component-playground-code">
-      <Highlight className="javascript">
-        {`
-          <Select
-            options={[
-              'Zoey Andrews',
-              'Jerry Woods',
-              'Marion Garza'
-            ]}
-            selected={2}
-            handleChange={(e) => { console.log(e.target.value); }}
-            label="Select user"
-          />
-        `}
-      </Highlight>
-    </div>
-  </div>;
+class Playground extends React.Component {
+  componentDidMount() {
+    hljs.configure({
+      classPrefix: ''
+    })
+    hljs.initHighlighting();
+  }
+  render() {
+    return (
+      <div className="react-component-playground">
+        { this.props.children }
+        <div className="react-component-playground-code">
+          <pre>
+            <code className="javascript">
+{`<Select
+  options={[
+    'Zoey Andrews',
+    'Jerry Woods',
+    'Marion Garza'
+  ]}
+  selected={2}
+  handleChange={(e) => { console.log(e.target.value); }}
+  label="Select user"
+/>`}
+            </code>
+          </pre>
+        </div>
+      </div>
+    );
+  }
+}
 
 Playground.propTypes = {
   children: PropTypes.node.isRequired

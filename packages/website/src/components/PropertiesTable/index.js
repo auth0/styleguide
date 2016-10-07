faaefaa0f8e0babbca8facdaa24c73ddf5bfe038
Ariel Gerstein <ariel.ger@hotmail.com>
2016-10-07 16:09:22 -0300

    Show arrayOf properties value.

--- packages/website/src/components/PropertiesTable/index.js
+++ packages/website/src/components/PropertiesTable/index.js
@@ -8,7 +8,7 @@ const PropertiesTable = ({ props }) => {
           <th width="15%">Property</th>
           <th width="15%">Type</th>
           <th width="60%">Description</th>
-          <th width="10%">Required</th>
+          <th width="10%" className="text-center">Required</th>
         </tr>
       </thead>
       <tbody>
@@ -17,9 +17,12 @@ const PropertiesTable = ({ props }) => {
             return (
               <tr key={prop}>
                 <td>{prop}</td>
-                <td>{props[prop].type.name}</td>
+                <td>
+                  {props[prop].type.name}
+                  {props[prop].type.name === 'arrayOf' && ` ${props[prop].type.value.name}`}
+                </td>
                 <td>{props[prop].description}</td>
-                <td>{props[prop].required}</td>
+                <td className="text-center">{props[prop].required && 'True'}</td>
               </tr>
             );
           })

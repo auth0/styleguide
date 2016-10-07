d5ec5925505749bf7b38af5f90698a15e6ece5b7
Ariel Gerstein <ariel.ger@hotmail.com>
2016-10-07 17:57:01 -0300

    Replace code-mirror for highlight component.

--- packages/website/src/components/Playground/index.js
+++ packages/website/src/components/Playground/index.js
@@ -1,25 +1,25 @@
 import React, { PropTypes } from 'react';
-import Codemirror from 'react-codemirror';
+import Highlight from 'react-highlight';
 import styles from './index.styl';
 
 const Playground = ({ children }) =>
   <div className="react-component-playground">
     { children }
     <div className="react-component-playground-code">
-      <Codemirror value={
-          `
-            <Select
-              options={[
-                'Zoey Andrews',
-                'Jerry Woods',
-                'Marion Garza'
-              ]}
-              selected={2}
-              handleChange={(e) => { console.log(e.target.value); }}
-              label="Select user"
-            />
-          `
-        } />
+      <Highlight className="javascript">
+        {`
+          <Select
+            options={[
+              'Zoey Andrews',
+              'Jerry Woods',
+              'Marion Garza'
+            ]}
+            selected={2}
+            handleChange={(e) => { console.log(e.target.value); }}
+            label="Select user"
+          />
+        `}
+      </Highlight>
     </div>
   </div>;
 

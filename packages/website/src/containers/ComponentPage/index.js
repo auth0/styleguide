92211616abdaf224d60c9cc5adaf4641dadd1a5a
Ariel Gerstein <ariel.ger@hotmail.com>
2016-10-07 14:36:13 -0300

    Mockup component page.

--- packages/website/src/containers/ComponentPage/index.js
+++ packages/website/src/containers/ComponentPage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './index.styl';
 // import { parse } from 'react-docgen';
 
 const ComponentPage = ({ component }) => {
@@ -6,10 +7,41 @@ const ComponentPage = ({ component }) => {
   // const parsedComments = parse(codeOfComponent);
 
   return (
-    <section>
-      <h1>React components</h1>
-
-      <h2>{component.title}</h2>
+    <section className="react-component-page">
+      <h1 className="component-title">{component.title}</h1>
+      <p className="component-description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, quos.
+      </p>
+      <table className="component-properties-table table table-fixed">
+        <thead>
+          <tr>
+            <th width="15%">Property</th>
+            <th width="15%">Type</th>
+            <th width="60%">Description</th>
+            <th width="10%">Required</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>array</td>
+            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, non?</td>
+            <td>true</td>
+          </tr>
+          <tr>
+            <td>Property</td>
+            <td>string</td>
+            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, non?</td>
+            <td>false</td>
+          </tr>
+          <tr>
+            <td>Options</td>
+            <td>array</td>
+            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, non?</td>
+            <td>false</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   );
 };

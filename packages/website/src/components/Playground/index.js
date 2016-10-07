f07606c0589e57464c61f5d9d0c335b44e18d034
Ariel Gerstein <ariel.ger@hotmail.com>
2016-10-07 17:40:07 -0300

    Add react-codemirror example for playground (wip)

--- packages/website/src/components/Playground/index.js
+++ packages/website/src/components/Playground/index.js
@@ -1,9 +1,26 @@
 import React, { PropTypes } from 'react';
+import Codemirror from 'react-codemirror';
 import styles from './index.styl';
 
 const Playground = ({ children }) =>
   <div className="react-component-playground">
     { children }
+    <div className="react-component-playground-code">
+      <Codemirror value={
+          `
+            <Select
+              options={[
+                'Zoey Andrews',
+                'Jerry Woods',
+                'Marion Garza'
+              ]}
+              selected={2}
+              handleChange={(e) => { console.log(e.target.value); }}
+              label="Select user"
+            />
+          `
+        } />
+    </div>
   </div>;
 
 Playground.propTypes = {

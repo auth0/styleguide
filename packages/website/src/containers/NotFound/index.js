8e263bd2a2cd7620806d4bd546b9dfec0b97f1e1
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2016-10-06 21:56:22 -0300

    Add error 404 page

--- packages/website/src/containers/NotFound/index.js
+++ packages/website/src/containers/NotFound/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import error404 from 'website-404-error-page/build/website-404-error-page.pug';
+import 'website-404-error-page/build/website-404-error-page.css';
 
-const NotFound = ({ component }) => (
-  <section>
-    <h1>Not Found!</h1>
-  </section>
+const NotFound = () => (
+  <section className="notfound-page" dangerouslySetInnerHTML={{ __html: error404() }} />
 );
 
 export default NotFound;

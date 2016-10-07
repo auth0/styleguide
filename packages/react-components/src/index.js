75726ad060232e48dbbb21292af204c4dfaa3340
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2016-10-06 22:18:27 -0300

    [react-components] fix lint errors

--- packages/react-components/src/index.js
+++ packages/react-components/src/index.js
@@ -1,1 +1,1 @@
-export Select from './components/Select';
+export Select from './Select';
--- packages/website/src/containers/ComponentPage/index.js
+++ packages/website/src/containers/ComponentPage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { parse } from 'react-docgen';
 
 const ComponentPage = ({ component }) => {
-  const codeOfComponent = require(`raw!auth0-styleguide-react-components/src/components/${component.component.name}/index.js`);
+  const codeOfComponent = require(`raw!auth0-styleguide-react-components/src/${component.component.name}/index.js`);
   const parsedComments = parse(codeOfComponent);
 
   return (

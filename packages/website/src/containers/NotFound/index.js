84c44becd2438f2515d9fc261c411f0a1569bc9c
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2016-10-11 01:55:09 -0300

    Fix heroku to run new  website

--- packages/website/src/containers/NotFound/index.js
+++ packages/website/src/containers/NotFound/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import error404 from 'website-404-error-page/build/website-404-error-page.pug';
-import 'website-404-error-page/build/website-404-error-page.css';
 
 const NotFound = () => (
-  <section className="notfound-page" dangerouslySetInnerHTML={{ __html: error404() }} />
+  <section className="notfound-page">Not found</section>
 );
 
 export default NotFound;

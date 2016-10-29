94d13b194c24bdfedbf4d06561cc24e8234b2474
Jenkins <jenkins@jenkins-us-west-1.int.auth0.com>
2016-10-29 23:26:15 +0000

    0.0.0 build

--- packages/react-components/src/examples.js
+++
@@ -1,3 +0,0 @@
-export Select from './Select/examples';
-export EmptyState from './EmptyState/examples';
-export TryBanner from './TryBanner/examples';
--- packages/react-website/src/containers/index.js
+++
@@ -1,7 +0,0 @@
-import App from 'containers/App';
-import NotFound from 'containers/NotFound';
-import ComponentEditor from 'containers/ComponentEditor';
-import ComponentPage from 'containers/ComponentPage';
-import Splash from 'containers/Splash';
-
-export { App, NotFound, ComponentEditor, ComponentPage, Splash };

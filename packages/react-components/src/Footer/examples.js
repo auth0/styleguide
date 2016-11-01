5fe15472b1b24fce13c364ac44af520c385518a3
Ariel Gerstein <ariel.ger@hotmail.com>
2016-11-01 18:27:54 -0300

    Add footer component.

---
+++ packages/react-components/src/Footer/examples.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './';
+
+const examples = [
+  {
+    component: (
+      <Footer />
+    ),
+    code: '<Footer />',
+    center: false,
+    title: 'Default',
+    showTitle: false,
+    url: 'default'
+  }
+];
+
+export default examples;
---
+++ packages/react-components/src/Footer/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import { Footer } from '../../src';
+
+storiesOf('Footer', module)
+  .add('default', () => (
+    <Footer />
+  ));

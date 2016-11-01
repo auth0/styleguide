f33ed688177846705920a0d0229f9389beaeac82
Ariel Gerstein <ariel.ger@hotmail.com>
2016-11-01 18:27:54 -0300

    Add footer component.

--- packages/react-components/src/examples.js
+++ packages/react-components/src/examples.js
@@ -1,3 +1,4 @@
 export Select from './Select/examples';
 export EmptyState from './EmptyState/examples';
 export TryBanner from './TryBanner/examples';
+export Footer from './Footer/examples';
--- packages/react-components/src/index.js
+++ packages/react-components/src/index.js
@@ -1,3 +1,4 @@
 export Select from './Select';
 export EmptyState from './EmptyState';
 export TryBanner from './TryBanner';
+export Footer from './Footer';

1ffb86b5db05bcc0815db2840a09c51d8e9586ba
Jenkins <jenkins@jenkins-us-west-1.int.auth0.com>
2016-11-23 20:15:40 +0000

    0.0.2 build

--- packages/website/src/html/containers/Home/index.js
+++
@@ -1,8 +0,0 @@
-import React, { PropTypes } from 'react';
-
-const Home = () =>
-  <div className="html-styleguide-section">
-    <h1>Home</h1>
-  </div>;
-
-export default Home;
--- packages/website/src/html/containers/Resources/index.js
+++
@@ -1,8 +0,0 @@
-import React, { PropTypes } from 'react';
-
-const Resources = () =>
-  <div className="html-styleguide-section">
-    <h1>Resources</h1>
-  </div>;
-
-export default Resources;

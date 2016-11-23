4f058d0a5d5d6b1d2a366f18977936e0ab4a2084
Ariel Gerstein <ariel.ger@hotmail.com>
2016-11-23 18:03:36 -0300

    [react-components] Fix sidebar example styles.

--- packages/react-components/src/Sidebar/examples.js
+++ packages/react-components/src/Sidebar/examples.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Sidebar from './';
+import sidebarExamples from './examples.json';
 
 const examples = [
   {
-    component: <Sidebar items={[]} />,
-    code: `<Sidebar items={[]}}>`,
+    component: <Sidebar items={sidebarExamples.plain} />,
+    code: `<Sidebar items={${JSON.stringify(sidebarExamples.plain)}}>`,
     title: 'Styleguide (HTML/CSS)',
     url: 'styleguide-html-css'
   }

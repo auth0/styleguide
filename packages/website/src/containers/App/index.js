566747574af11a1f6a664db4158bb8e1075df13c
Ariel Gerstein <ariel.ger@hotmail.com>
2016-11-24 15:34:41 -0300

    [website] Fix html styleguide routing with regex

--- packages/website/src/containers/App/index.js
+++ packages/website/src/containers/App/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { HashRouter, Match, Miss } from 'react-router';
+import { HashRouter, Match } from 'react-router';
 import { App as HtmlStyleguide } from 'html/containers';
 import { App as ReactStyleguide } from 'react/containers';
+
+const reactRegex = /^\/react(\/.*)?$/;
 
 const App = () => (
   <HashRouter>
     <div>
-      <Match pattern="/react" component={ReactStyleguide} />
-      <Miss component={HtmlStyleguide} />
+      <Match
+        pattern=""
+        render={({ location }) => {
+          console.log(location);
+          return reactRegex.test(location.pathname) ? <ReactStyleguide /> : <HtmlStyleguide />;
+        }}
+      />
     </div>
   </HashRouter>
 );

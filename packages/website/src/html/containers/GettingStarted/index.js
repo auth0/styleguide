5b7a49b0114d8b1843c440336c6c780dd65d04c7
Ariel Gerstein <ariel.ger@hotmail.com>
2016-11-25 12:20:56 -0300

    [website] Highlight getting started code snippets.

--- packages/website/src/html/containers/GettingStarted/index.js
+++ packages/website/src/html/containers/GettingStarted/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import hljs from 'highlight.js';
+import $ from 'jquery';
 import gettingStartedIcon from '../../img/getting-started-icon.svg';
 
 const cdnExample = '<link href="https://cdn.auth0.com/styleguide/0.0.1/index.min.css" rel="stylesheet" />';
@@ -8,41 +10,67 @@ cd styleguide
 grunt dev`
 );
 
-const GettingStarted = () =>
-  <div className="styleguide-section getting-started-section">
-    <section className="jumbotron">
-      <h2 dangerouslySetInnerHTML={{ __html: gettingStartedIcon }} />
-      <h1>Getting started</h1>
-      <p>There are few ways to initialize depending on the use case.</p>
-    </section>
-    <div className="section-content">
-      <section>
-        <h2 id="from-cdn">From CDN</h2>
-        <p>
-          Using our CDN is the simplest solution.
-          Just add the following lines in your projects HTML head and you are done.
-        </p>
-        <pre><hl>{cdnExample}</hl></pre>
-      </section>
-      <section>
-        <h2 id="from-gh">Installing from Github</h2>
-        <p>
-          Clone the repository to your computer. If you want more information
-          <a
-            href="https://github.com/auth0/styleguide/blob/master/README.md"
-            target="_blank" rel="noopener noreferrer"
-          >
-            read here.
-          </a>
-        </p>
-        <pre className="hl">
-          <code className="html">
-            {githubExample}
-          </code>
-        </pre>
-      </section>
-    </div>
-  </div>;
+class GettingStarted extends Component {
+  constructor() {
+    super();
+
+    this.highlightSnippets = this.highlightSnippets.bind(this);
+  }
+  componentDidMount() {
+    console.log('h');
+    this.highlightSnippets();
+  }
+  highlightSnippets() {
+    hljs.configure({
+      tabReplace: '  ', // 4 spaces
+      classPrefix: ''     // don't append class prefix
+    });
+
+    hljs.initHighlighting();
+
+    $('pre.hl code').each((i, block) => (hljs.highlightBlock(block)));
+  }
+  render() {
+    return (
+      <div className="styleguide-section getting-started-section">
+        <section className="jumbotron">
+          <h2 dangerouslySetInnerHTML={{ __html: gettingStartedIcon }} />
+          <h1>Getting Started</h1>
+          <p>There are few ways to initialize depending on the use case.</p>
+        </section>
+        <div className="section-content">
+          <section>
+            <h2 id="from-cdn">From CDN</h2>
+            <p>
+              Using our CDN is the simplest solution.
+              Just add the following lines in your projects HTML head and you are done.
+            </p>
+            <pre className="hl">
+              <code className="html">{cdnExample}</code>
+            </pre>
+          </section>
+          <section>
+            <h2 id="from-gh">Installing from Github</h2>
+            <p>
+              Clone the repository to your computer. If you want more information
+              <a
+                href="https://github.com/auth0/styleguide/blob/master/README.md"
+                target="_blank" rel="noopener noreferrer"
+              >
+                read here.
+              </a>
+            </p>
+            <pre className="hl">
+              <code className="html">
+                {githubExample}
+              </code>
+            </pre>
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default GettingStarted;
 

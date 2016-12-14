108f57df8fe4c74dd23fb096046aff492f38a4c5
Ariel Gerstein <ariel.ger@hotmail.com>
2016-12-14 16:54:04 -0300

    [react-components] Release: 0.0.3

--- packages/react-components/src/Button/examples.js
+++ packages/react-components/src/Button/examples.js
@@ -29,25 +29,25 @@ const examples = [
     component: (
       <div>
         {/* Standard button */}
-        <Button>Default</Button>
+        <Button>Default</Button>&nbsp;&nbsp;
 
         {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-        <Button bsStyle="primary">Primary</Button>
+        <Button bsStyle="primary">Primary</Button>&nbsp;&nbsp;
 
         {/* Indicates a successful or positive action */}
-        <Button bsStyle="success">Success</Button>
+        <Button bsStyle="success">Success</Button>&nbsp;&nbsp;
 
         {/* Contextual button for informational alert messages */}
-        <Button bsStyle="info">Info</Button>
+        <Button bsStyle="info">Info</Button>&nbsp;&nbsp;
 
         {/* Indicates caution should be taken with this action */}
-        <Button bsStyle="warning">Warning</Button>
+        <Button bsStyle="warning">Warning</Button>&nbsp;&nbsp;
 
         {/* Indicates a dangerous or potentially negative action */}
-        <Button bsStyle="danger">Danger</Button>
+        <Button bsStyle="danger">Danger</Button>&nbsp;&nbsp;
 
         {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
-        <Button bsStyle="link">Link</Button>
+        <Button bsStyle="link">Link</Button>&nbsp;&nbsp;
       </div>
     ),
     code: codeExample1,

c9889bfc7e3925bfe4992a19f0e68dadac66fa50
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2016-12-18 05:42:17 -0300

    [website] Make table of properties more prettier and fix stylus imports of core

--- packages/website/src/react/components/PropertiesTable/index.js
+++ packages/website/src/react/components/PropertiesTable/index.js
@@ -1,35 +1,44 @@
 import React, { PropTypes } from 'react';
+import cx from 'classnames';
+import './index.styl';
 
-const PropertiesTable = ({ props }) =>
-  <div className="table-responsive">
-    <table className="component-properties-table table">
-      <thead>
-        <tr>
-          <th>Property</th>
-          <th>Type</th>
-          <th>Description</th>
-          <th>Default value</th>
-          <th className="text-center">Required</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          Object.keys(props).map((prop) =>
-            <tr key={prop}>
-              <td>{prop}</td>
-              <td>
-                {props[prop].type.name}
-                {props[prop].type.name === 'arrayOf' && ` ${props[prop].type.value.name}`}
-              </td>
-              <td>{props[prop].description}</td>
-              <td>{props[prop].defaultValue ? props[prop].defaultValue.value : '' }</td>
-              <td className="text-center">{props[prop].required && 'True'}</td>
+const PropertiesTable = ({ props }) => {
+  let thereAreIsRequiredProp = false;
+  return (
+    <div className="component-properties">
+      <div className="table-responsive component-properties-table-container">
+        <table className="component-properties-table table">
+          <thead>
+            <tr>
+              <th>Property</th>
+              <th>Type</th>
+              <th>Description</th>
+              <th>Default value</th>
             </tr>
-          )
-        }
-      </tbody>
-    </table>
-  </div>;
+          </thead>
+          <tbody>
+            {Object.keys(props).map((prop) => {
+              const requiredProp = props[prop].required;
+              if (requiredProp) thereAreIsRequiredProp = true;
+              return (
+                <tr key={prop}>
+                  <td className={cx({ required: requiredProp })}>{prop}</td>
+                  <td>
+                    {props[prop].type.name}
+                    {props[prop].type.name === 'arrayOf' && ` ${props[prop].type.value.name}`}
+                  </td>
+                  <td>{props[prop].description}</td>
+                  <td>{props[prop].defaultValue ? props[prop].defaultValue.value : '' }</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      {thereAreIsRequiredProp && <p className="required-message">Required property</p>}
+    </div>
+  );
+};
 
 PropertiesTable.propTypes = {
   props: PropTypes.object.isRequired

778eeb432212df671274a687f8113208b31d0465
Ariel Gerstein <ariel.ger@hotmail.com>
2016-12-19 12:25:30 -0300

    [website] Add type boxes to Design section.

--- packages/website/src/html/components/index.js
+++ packages/website/src/html/components/index.js
@@ -4,3 +4,4 @@ export Badge from './Badge';
 export Icon from './Icon';
 export SVG from './SVG';
 export Sidebar from './Sidebar';
+export TypeBox from './TypeBox';
--- packages/website/src/html/containers/Design/index.js
+++ packages/website/src/html/containers/Design/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import DesignIcon from '../../img/design-icon.svg';
-import { SVG } from '../../components';
+import { SVG, TypeBox } from '../../components';
+import './index.styl';
 
 const Design = () =>
-  <div className="styleguide-section">
+  <div className="styleguide-section design-section">
     <section className="jumbotron">
       <h2><SVG content={DesignIcon} /></h2>
       <h1>Design</h1>
@@ -11,7 +12,12 @@ const Design = () =>
     </section>
     <section className="section-content">
       <section>
-        <h2>Typography</h2>
+        <h2 id="typography">Typography</h2>
+        <p>The body font is set to Nitti Grotesk, font size is 16px and line height is 2.</p>
+        <div className="row type-box-container">
+          <TypeBox />
+          <TypeBox themeDark />
+        </div>
       </section>
       <section>
         <h2>Primary colors</h2>

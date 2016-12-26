98e2f69b5a3e15ff912ea9960cdb0112da0097e7
Jenkins <jenkins@jenkins-us-west-1.int.auth0.com>
2016-12-26 18:46:38 +0000

    0.0.3 build

--- packages/website/src/react/components/index.js
+++
@@ -1,4 +0,0 @@
-export Example from 'react/components/Example';
-export PropertiesTable from 'react/components/PropertiesTable';
-export Sidebar from 'react/components/Sidebar';
-export CodepenPlayground from 'react/components/CodepenPlayground';

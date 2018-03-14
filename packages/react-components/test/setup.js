b0fb8e14f544234d0b3167fac3e6426f7bcb511e
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2018-03-14 01:01:21 +0100

    update enzyme to match react 16

--- packages/react-components/test/setup.js
+++ packages/react-components/test/setup.js
@@ -1,4 +1,8 @@
 /* Configure Mocha test runner, see package.json/scripts/test */
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 process.env.NODE_ENV = 'test';
 

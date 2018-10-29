60bb17125bfc7201113fedbdf9301258a8d2da5f
Christian Fleschhut <weltrekordhalter@gmail.com>
2018-10-29 17:14:52 +0100

    refactor(button-stories): use toolbar component to preserve spacing between buttons

--- packages/react-components/src/Button/index.stories.js
+++ packages/react-components/src/Button/index.stories.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import centered from '@kadira/react-storybook-decorator-centered';
-import { Button } from '../../src';
+import { Button, ButtonToolbar } from '../../src';
 
-storiesOf('Button', module).addDecorator(centered).add('Button styles', () =>
-  <div>
-    {/* Standard button */}
-    <Button>Default</Button> &nbsp;&nbsp;
+storiesOf('Button', module)
+  .addDecorator(centered)
 
-    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-    <Button bsStyle="primary">Primary</Button> &nbsp;&nbsp;
+  .add('Button styles', () => (
+    <ButtonToolbar>
+      {/* Standard button */}
+      <Button>Default</Button>
 
-    {/* Indicates a successful or positive action */}
-    <Button bsStyle="success">Success</Button> &nbsp;&nbsp;
+      {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+      <Button bsStyle="primary">Primary</Button>
 
-    {/* Contextual button for informational alert messages */}
-    <Button bsStyle="info">Info</Button> &nbsp;&nbsp;
+      {/* Indicates a successful or positive action */}
+      <Button bsStyle="success">Success</Button>
 
-    {/* Indicates caution should be taken with this action */}
-    <Button bsStyle="warning">Warning</Button> &nbsp;&nbsp;
+      {/* Contextual button for informational alert messages */}
+      <Button bsStyle="info">Info</Button>
 
-    {/* Indicates a dangerous or potentially negative action */}
-    <Button bsStyle="danger">Danger</Button> &nbsp;&nbsp;
+      {/* Indicates caution should be taken with this action */}
+      <Button bsStyle="warning">Warning</Button>
 
-    {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
-    <Button bsStyle="link">Link</Button>
-  </div>
-);
+      {/* Indicates a dangerous or potentially negative action */}
+      <Button bsStyle="danger">Danger</Button>
+
+      {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+      <Button bsStyle="link">Link</Button>
+    </ButtonToolbar>
+  ));
